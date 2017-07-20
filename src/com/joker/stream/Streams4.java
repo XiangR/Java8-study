7c14b8ca7f5366c9fa52a906f22cb38a8c09d9bc
Rui Xiang <xiangruimx@163.com>
2017-07-20 10:16:38 +0800

    range
    理解range与rangeClosed

--- src/com/joker/stream/Streams4.java
+++ src/com/joker/stream/Streams4.java
@@ -3,8 +3,10 @@ package com.joker.stream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalInt;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * 
@@ -38,6 +40,8 @@ public class Streams4 {
 		int sum1 = getIntegerList(100).parallelStream().mapToInt(a -> a).sum();
 		int sum2 = getIntegerList2(100).parallelStream().mapToInt(a -> a).sum();
 		System.out.println(String.format("sum1: %d; sum2: %d", sum1, sum2));
+
+		rangeDistinguish();
 	}
 
 	/**
@@ -65,11 +69,33 @@ public class Streams4 {
 	private static List<Integer> getIntegerList2(Integer count) {
 		count = count == null || count == 0 ? 50 : count;
 
+		List<Integer> collect = IntStream.range(0, count).boxed().collect(Collectors.toList());
+		return collect;
+	}
+
+	/*
+	 * 理解range rangeClosed
+	 */
+	public static void rangeDistinguish() {
+
 		/*
 		 * range 的使用类似于一个for循环的遍历但是不包含最后一个数
 		 * 可理解为 i = 0; i < count; i ++
 		 */
-		List<Integer> collect = IntStream.range(0, count).boxed().collect(Collectors.toList());
-		return collect;
+		Supplier<Stream<Integer>> supplierRange = () -> IntStream.range(0, 10).boxed();
+		supplierRange.get().forEach(k -> {
+			System.out.println(String.format("range: %d", k));
+		});
+		System.out.println(String.format("count -> range: %d", supplierRange.get().count()));
+		
+		/*
+		 * rangeClosed 的使用
+		 * 可理解为 i = 0; i <= count; i ++
+		 */
+		Supplier<Stream<Integer>> supplierRangeClosed = () -> IntStream.rangeClosed(0, 10).boxed();
+		supplierRangeClosed.get().forEach(k -> {
+			System.out.println(String.format("rangeClosed: %d", k));
+		});
+		System.out.println(String.format("count -> rangeClosed: %d", supplierRangeClosed.get().count()));
 	}
 }
